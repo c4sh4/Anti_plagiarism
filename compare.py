4368040d229a23c2ac388d59c052e8830b9f59fc
Udalov Anton <nt.dlv@list.ru>
2023-01-07 16:55:09 +0300

    Final version of plagiarism detector

--- compare.py
+++ compare.py
@@ -3,15 +3,16 @@ import io
 import tokenize
 import token
 import dis
+import argparse
+import time
 import pandas as pd
 
 
-def input_txt():
+def input_txt(input_path):
     """ Open  file with a list of pairs of documents."""
     # 'input.txt' r'C:\Users\Admin\Desktop\Tinkoff_edu\test.txt '
-    with open('input.txt', 'r', encoding='utf-8') as input_file:
+    with open(str(input_path), 'r', encoding='utf-8') as input_file:
         in_path = input_file.read().split()
-        # print(len(in_path[:50]))
     return in_path
 
 
@@ -53,9 +54,9 @@ def longest_common_subsequence(sel1, sel2):
     """ Longest common subsequence."""
     size1 = len(sel1)
     size2 = len(sel2)
-    # if size1 < size2:
-    #   sel1, sel2 = sel2, sel1
-    #   size1, size2 = size2, size1
+    if size1 < size2:
+        sel1, sel2 = sel2, sel1
+        size1, size2 = size2, size1
     if size1 * size2 == 0:
         return 0
     curr = [0] * (1 + size2)
@@ -113,10 +114,7 @@ def code_similarity(f_list):
             lcs_list.append(round(lcs / len(f_list[p]), 3))
         else:
             lcs_list.append(round(1 - (dl_d / len(f_list[p])), 3))
-        print('step: ', p)
         p += 2
-    # print('dl_list size is: ', len(dl_list))
-    # print('lcs size is: ', len(lcs_list))
     temporary_dict = {'dl': dl_list, 'lcs': lcs_list}
     code_sim_df = pd.DataFrame(temporary_dict)
     return code_sim_df
@@ -127,7 +125,6 @@ def token_column(lex_list):
     token_list = []
     for tok in enumerate(lex_list):
         token_list.append(list(lex_list[tok[0]]['token']))
-    # print('len of token_list is: ', len(token_list))
     return code_similarity(token_list)
 
 
@@ -138,72 +135,83 @@ def lexeme_column(lex_list):
         lex_list[lexeme[0]] = lex_list[lexeme[0]][lex_list[lexeme[0]]['token'] != 'STRING']
     for lex_ in enumerate(lex_list):
         s_lex_list.append(list(lex_list[lex_[0]]['lexeme']))
-    # print('len of lex_list is: ', len(s_lex_list))
     return code_similarity(s_lex_list)
 
 
-def scores(res_list):
-    """ Fill scores.txt."""
-    with open('scores.txt', 'w', encoding='utf-8') as score:
-        for res_ in res_list:
-            score.write(str(res_) + '\n')
-
-
-if __name__ == "__main__":
-    file_list = []
+def get_scores(f_list, pair_list):
+    """ Scoring representations of a pair of files in the form of byte code, tokens and program text.
+    Scoring is based on the Damerau Levenshtein distance and the largest common subsequence.
+    """
     lexeme_list = []
     bytecode_list = []
-    pair_list = []
-    path_list = input_txt()
-    for i, path in enumerate(path_list):
-        file_list.append(get_file(path))
-        if i % 2 == 1:
-            pair_list.append(i)
-    # print('file list size:', len(file_list))
-
-    for fc in file_list:
+    for fc in f_list:
         lexeme_list.append(lexeme_selection(fc))
         bytecode_list.append(bytecode_selection(fc))
     df = pd.DataFrame(pair_list)
-
-    # print('bytecode: ')
     df = df.join(code_similarity(bytecode_list))
     df = df.rename(columns={0: "f", "dl": "b_dl", "lcs": "b_lcs"})
-    # print('tokens: ')
-
     df = df.join(token_column(lexeme_list))
     df = df.rename(columns={"dl": "t_dl", "lcs": "t_lcs"})
     df['distance'] = abs(df.b_dl - df.t_lcs)
-
+    # Replacement for file pairs for which binary representations were not obtained.
     df.loc[(df['b_dl'] == 0), 'b_dl'] = df.t_dl
-
-    df_doubt = df[(df['distance'] > 0.150)]
-    short_lexeme_list = []
-    for f in df_doubt.f:
-        short_lexeme_list.append(lexeme_list[int(f) - 1])
-        short_lexeme_list.append(lexeme_list[int(f)])
-
-    df_doubt = df_doubt.reset_index()
-    df_doubt = df_doubt.join(lexeme_column(short_lexeme_list))
-    df_doubt = df_doubt.rename(columns={"dl": "l_dl", "lcs": "l_lcs"})
-
-    # df = df.join(lexeme_column(lexeme_list))
-    # df = df.rename(columns={"dl": "l_dl", "lcs": "l_lcs"})
-    df_doubt = df_doubt.drop(columns='index')
+    # additional test
+    doubt = additional_test(df[(df['distance'] > 0.150)], lexeme_list)
     df['l_dl'] = df['b_dl']
     df['l_lcs'] = df['t_lcs']
-
-    for ind, r in enumerate(df_doubt['f']):
-        df.loc[(df['f'] == r), 'l_dl'] = df_doubt['l_dl'][ind]
-        df.loc[(df['f'] == r), 'l_lcs'] = df_doubt['l_lcs'][ind]
-        # df[(df['f'] == r)]['l_dl'] = df_doubt[(df_doubt['f'] == r)]['l_dl']
-        # print(df_doubt['l_dl'][ind])
-
+    for ind, r in enumerate(doubt['f']):
+        df.loc[(df['f'] == r), 'l_dl'] = doubt['l_dl'][ind]
+        df.loc[(df['f'] == r), 'l_lcs'] = doubt['l_lcs'][ind]
     df = df.drop(columns='distance')
-    # print(df)
     res = round((df['b_dl'] + df['t_lcs'] + df['l_dl'] + df['l_lcs']) / 4, 3)
     for r in enumerate(res):
         res[r[0]] = res[r[0]] if res[r[0]] > 0 else 0.
     result_list = list(res)
-    print(result_list)
-    scores(result_list)
+    return result_list
+
+
+def additional_test(df_doubt, lexeme_list):
+    """ Checking pairs of texts on which the byte code similarity check gave a doubtful result or was not passed.
+    Files are compared directly for each token, with the exclusion of comments.
+    """
+    short_lexeme_list = []
+    for f in df_doubt.f:
+        short_lexeme_list.append(lexeme_list[int(f) - 1])
+        short_lexeme_list.append(lexeme_list[int(f)])
+    df_doubt = df_doubt.reset_index()
+    df_doubt = df_doubt.join(lexeme_column(short_lexeme_list))
+    df_doubt = df_doubt.rename(columns={"dl": "l_dl", "lcs": "l_lcs"})
+    df_doubt = df_doubt.drop(columns='index')
+    return df_doubt
+
+
+def scores_file(res_list, scores_path):
+    """ Fill scores.txt."""
+    with open(str(scores_path), 'w', encoding='utf-8') as score:
+        for res_ in res_list:
+            score.write(str(res_) + '\n')
+
+
+def parser():
+    pars = argparse.ArgumentParser(description='')
+    pars.add_argument('input', type=str, help='Input file with a list of file pairs to check.')
+    pars.add_argument('scores', type=str, help='Program text similarity evaluation file.')
+    args = pars.parse_args()
+    source_ = (args.input, args.scores)
+    return source_
+
+
+if __name__ == "__main__":
+    start = time.time()
+    file_list = []
+    list_of_couples = []
+    source = parser()
+    path_list = input_txt(source[0])
+    for i, path in enumerate(path_list):
+        file_list.append(get_file(path))
+        if i % 2 == 1:
+            list_of_couples.append(i)
+    score_list = get_scores(file_list, list_of_couples)
+    scores_file(score_list, source[1])
+    end = time.time()-start
+    print('time of evaluation: ', round(end, 3))
